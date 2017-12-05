c933db1761d57353e07e0758faa39cb30dfffaa2
Alex Cota <cota.alexandra14@gmail.com>
2017-12-05 11:20:41 -0800

    setup

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">PuppyPals2</h1>
+        </header>
+        <p></p>
+      </div>
+    );
+  }
+}
+
